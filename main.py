bf57fc004e9a0e442422a2c4f70ea58df89107a8
Maksym <PeterParker@gmail.com>
2025-07-15 23:00:19 +0300

    homework done

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print(2222)
+# робимо зміну
+address = "Rybnitsa\U0001f3e0"
+flag = "\U0001f1f2"
+print(ord(flag))
+smart_flag = "flag 127474"
+print(smart_flag)
+smart_flag2 = f"{flag} 127474"
+print(smart_flag2)
