36ce4aa38e8c75a6f92011519dc90002a0f9508e
Maksym <PeterParker@gmail.com>
2025-07-23 19:26:31 +0300

    homework4 is done

--- main.py
+++ main.py
@@ -1,8 +1,65 @@
-# робимо зміну
-address = "Rybnitsa\U0001f3e0"
-flag = "\U0001f1f2"
-print(ord(flag))
-smart_address = address + " " + flag
-print(smart_address)
-smart_flag2 = f"{address} {flag}"
-print(smart_flag2)
+first_bank_bill = 3500
+second_bank_bill = 1250
+together_in_two_banks = first_bank_bill + second_bank_bill
+print(together_in_two_banks)
+
+quantity = 23
+price = 17
+cost = 23 * 17
+print(cost)
+
+my_money = 58
+price_of_ice_cream = 8
+ice_cream_i_can_buy = my_money // price_of_ice_cream
+print(ice_cream_i_can_buy)
+
+some_number = 250.0
+op = some_number.is_integer()
+print(op)
+
+one_side = 12
+square_of_square = one_side**2
+print(square_of_square)
+
+price_of_pizza = 340
+everyone_may_to_pay = price_of_pizza / 2
+print(everyone_may_to_pay)
+
+phone_number = "     0991234567 "
+normal_phone_number = phone_number.strip()
+print(normal_phone_number)
+
+
+cost_of_product = "2500"
+cost_of_product = float(cost_of_product)
+final_cost = cost_of_product + 300
+print(final_cost)
+
+registration = input("Enter your name ")
+print(registration.upper())
+
+email1 = "Test@Gmail.com"
+email2 = "test@gmail.com"
+email3 = email1 == email2
+print(email3)
+
+year1 = 2020
+year2 = 2025
+if year1 > year2:
+    print("Current year is 2020")
+else:
+    print("Current year is 2025")
+
+one_side1 = 5
+square_of_cube = 5**3
+print(square_of_cube)
+
+quantity_of_seconds = 12345
+quantity_of_hours = quantity_of_seconds // 3600
+print(quantity_of_hours)
+
+some_number2 = 987
+if some_number2 % 3 == 0:
+    print("True")
+else:
+    print("False")
