3194cf400f09d9fd408c2caad426dbb6af3dd999
Maksym <PeterParker@gmail.com>
2025-07-26 20:52:26 +0300

    homework5 is done

---
+++ lists.py
@@ -0,0 +1,26 @@
+text = "one two three four five"
+print(text.split())
+
+line = "111-222-333-444"
+print(line.split("-"))
+
+my_list = ["dog", "cat", "bird"]
+joined_list = ", ".join(my_list)
+print(joined_list)
+
+my_hobbies = ["playing guitar", "paying chess", "riding bike"]
+print(my_hobbies[0], my_hobbies[2])
+
+numbers = ["1", "2", "3", "4", "5"]
+print(numbers[1:4])
+
+strings = f"234235346456" f"dfhfgjghutyu"
+print(strings)
+
+phrase = "abc def ghi"
+print(phrase[:5], phrase[-3:])
+
+
+colors = ["red", "green", "blue", "yellow"]
+concreted_colours = colors[0], colors[1], colors[3]
+print(concreted_colours)
